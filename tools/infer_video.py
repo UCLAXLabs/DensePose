046453a17f1b87f20566789c0b5d1579eaa4e0c6
Peter Broadwell <broadwell@library.ucla.edu>
2018-10-19 17:07:53 -0700

    Script for UCLA K-pop project

---
+++ tools/infer_video.py
@@ -0,0 +1,194 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+##############################################################################
+
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+
+# PMB extra imports for video processing
+import numpy as np
+
+from caffe2.python import workspace
+
+from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.utils.io import cache_url
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
+import detectron.core.test_engine as infer_engine
+import detectron.datasets.dummy_datasets as dummy_datasets
+import detectron.utils.c2 as c2_utils
+import detectron.utils.vis as vis_utils
+
+c2_utils.import_detectron_ops()
+
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='End-to-end inference')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+        default=None,
+        type=str
+    )
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+        default=None,
+        type=str
+    )
+    parser.add_argument(
+        '--output-dir',
+        dest='output_dir',
+        help='directory for visualization pdfs (default: /tmp/infer_simple)',
+        default='/tmp/infer_simple',
+        type=str
+    )
+    parser.add_argument(
+        '--image-ext',
+        dest='image_ext',
+        help='image file name extension (default: jpg)',
+        default='jpg',
+        type=str
+    )
+    parser.add_argument(
+        '--video',
+        dest="video",
+        help='video file', 
+        default=None,
+        type=str
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+
+def main(args):
+    logger = logging.getLogger(__name__)
+    merge_cfg_from_file(args.cfg)
+    # Detectron is only able to use 1 GPU for inference
+    cfg.NUM_GPUS = 1
+    args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
+    assert_and_infer_cfg(cache_urls=False)
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+
+    if (not os.path.isfile(args.video)):
+        print("Unable to find video",args.video)
+        sys.exit(1)
+
+    videofile = args.video
+    cap = cv2.VideoCapture(videofile)
+
+    fps = cap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
+    multiplier = int(round(fps)) 
+
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+        sys.exit(1)
+
+    i = 0
+
+    while(cap.isOpened()):
+        ret_val, im = cap.read()
+
+        frameId = int(round(cap.get(1)))
+
+        print("frameID",frameId,"multiplier",multiplier)
+
+        fps_time = time.time()
+
+        im_name = str(fps_time)
+        
+        out_name = os.path.join(
+            args.output_dir, '{}'.format(im_name + '.jpg')
+        )
+
+        logger.info('Processing {}'.format(im_name))
+        
+        timers = defaultdict(Timer)
+        t = time.time()
+        with c2_utils.NamedCudaScope(0):
+            cls_boxes, cls_segms, cls_keyps, cls_bodys = infer_engine.im_detect_all(
+                model, im, None, timers=timers
+            )
+        logger.info('Inference time: {:.3f}s'.format(time.time() - t))
+        for k, v in timers.items():
+            logger.info(' | {}: {:.3f}s'.format(k, v.average_time))
+        if i == 0:
+            logger.info(
+                ' \ Note: inference on the first image will be slower than the '
+                'rest (caches and auto-tuning need to warm up)'
+            )
+        i += 1
+
+        vis_utils.vis_only_figure(
+            im[:, :, ::-1],  # BGR -> RGB for visualization
+            im_name,
+            out_name,
+            cls_boxes,
+            cls_segms,
+            cls_keyps,
+            cls_bodys,
+            dataset=dummy_coco_dataset,
+            box_alpha=0.3,
+            show_class=True
+        )
+        """
+        vis_utils.vis_one_image(
+            im[:, :, ::-1],  # BGR -> RGB for visualization
+            im_name,
+            args.output_dir,
+            cls_boxes,
+            cls_segms,
+            cls_keyps,
+            cls_bodys,
+            dataset=dummy_coco_dataset,
+            box_alpha=0.3,
+            show_class=True,
+            thresh=0.7,
+            kp_thresh=2
+        )
+        """
+
+        #cv2.putText(im,
+        #            "FPS: %f" % (1.0 / (time.time() - fps_time)),
+        #            (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+        #            (0, 255, 0), 2)
+        #    #cv2.imshow('tf-pose-estimation result', image)
+        #cv2.imwrite(out_name, kp_img)
+
+        #fps_time = time.time()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    setup_logging(__name__)
+    args = parse_args()
+    main(args)
